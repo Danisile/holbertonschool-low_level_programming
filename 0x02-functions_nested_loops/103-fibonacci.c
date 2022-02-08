9c02ae1a25b01b7884a6a66caf4eeb7d09d2412a
Danisile Johannah <77388022+Danisile@users.noreply.github.com>
2022-02-08 09:23:35 +0200

    Second commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,18 +4,19 @@ int main(void)
 {
 	double a = 0;
 	double b = 1;
+	double sum = 0;
 	double f;
-	int i;
+	int i, pair;
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 32; i++)
 	{
 		f = a + b;
-		if (i != 49)
-			printf("%.f, ", f);
-		else
-			printf("%.f\n", f);
+		pair = f;
+		if (pair % 2 == 0)
+			sum = sum + f;
 		a = b;
 		b = f;
 	}
+	printf("%.f\n", sum);
 	return (0);
 }
